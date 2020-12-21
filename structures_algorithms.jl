e673ceb424016e8de7faf34cf6775e6d11e6fbd4
Mladen Gibanica <mgcth@users.noreply.github.com>
2020-12-21 18:24:26 +0100

    more structures added

--- structures_algorithms.jl
+++ structures_algorithms.jl
@@ -4,12 +4,238 @@ using Random
 
 export Node, RMHeap, add!, remove!, meld!
 
+import Base.push!, Base.pop!
+
+"""
+    LinkedList
+
+Linked list supertype
+"""
+abstract type LinkedList{T} end
+
+
 """
     NodeType
 
 Holds types a Node can take.
 """
 NodeType = Union{T, Nothing} where T
+
+
+"""
+    DynamicArray
+
+A dynamic aray implementation. 
+"""
+mutable struct DynamicArray{T <: AbstractArray}
+    size::Int64
+    array::T
+end
+
+
+"""
+    SLLNode
+
+Single linked list node. Holds pointer to next node and some value.
+"""
+mutable struct SLLNode{T}
+    next::SLLNode{T}
+    value::T
+    SLLNode{T}() where T = (x = new(); x.next = x; x)
+    SLLNode{T}(y::T) where T = (x = new(); x.next = x; x.value = y; x)
+    SLLNode{T}(n::SLLNode{T}, x::T) where T = new(n, x)
+end
+
+"""
+    DLLNode
+
+Double linked list node. Holds pointers to previous and next nodes and some value.
+"""
+mutable struct DLLNode{T}
+    prev::DLLNode{T}
+    next::DLLNode{T}
+    value::T
+    DLLNode{T}() where T = (x = new(); x.prev = x; x.next = x; x)
+    DLLNode{T}(y::T) where T = (x = new(); x.prev = x; x.next = x; x.value = y; x)
+    DLLNode{T}(p::DLLNode{T}, n::DLLNode{T}, x::T) where T = new(p, n, x)
+end
+
+"""
+    SLinkedList
+
+A single linked list implementation.
+"""
+mutable struct SLinkedList{T} <: LinkedList{T}
+    node::SLLNode{T}
+    size::Int
+    SLinkedList{T}() where T = new(SLLNode{T}(), 1)
+    SLinkedList{T}(x::T) where T = new(SLLNode{T}(x), 1)
+end
+
+"""
+    DLinkedList
+
+A double linked list implementation.
+"""
+mutable struct DLinkedList{T} <: LinkedList{T}
+    node::DLLNode{T}
+    size::Int
+    DLinkedList{T}() where T = new(DLLNode{T}(), 1)
+    DLinkedList{T}(x::T) where T = new(DLLNode{T}(x), 1)
+end
+
+"""
+    addlast!(l, x)
+
+Add node to last position in linked list.
+"""
+function addlast!(l::SLinkedList{T}, x::T) where T
+    n = l.node
+    l.size = l.size
+    while n != n.next
+        n = n.next
+    end
+
+    l.size += 1
+    n.value = x
+    n.next = SLLNode{T}()
+    return nothing
+end
+
+function addlast!(l::DLinkedList{T}, x::T) where T
+    n = l.node
+    l.size = l.size
+    while n != n.next
+        n = n.next
+    end
+
+    l.size += 1
+    n.value = x
+    n.next = DLLNode{T}()
+    n.next.prev = n
+    return nothing
+end
+
+"""
+    addfirst!(l, x)
+
+Add node to first position in linked list.
+"""
+function addfirst!(l::DLinkedList{T}, x::T) where T
+    n = l.node
+    next = n.next
+    n = DLLNode{T}(x)
+    n.next = next
+    l.size += 1
+    return nothing
+end
+
+"""
+    add!(l, x, pos = 0)
+
+Add node to linked list.
+"""
+function add!(l::SLinkedList, x::T, pos::Int = l.size) where T
+    n = l.node
+    i = 1
+    while i != pos
+        n = n.next
+        i += 1
+    end
+
+    next = n.next
+    n = SLLNode{T}(x)
+    n.next = next
+    l.size += 1
+    return nothing
+end
+
+function add!(l::DLinkedList, x::T, pos::Int = l.size) where T
+    n = l.node
+    i = 1
+    while i != pos
+        n = n.next
+        i += 1
+    end
+
+    next = n.next
+    prev = n.prev
+    n = DLLNode{T}(x)
+    n.next = next
+    n.prev = prev
+    l.size += 1
+    return nothing
+end
+
+"""
+    remove!(l)
+
+Remove last node from linked list.
+"""
+function remove!(l::SLinkedList{T}) where T
+    n = l.node
+    p = l.node
+    while n != n.next
+        p = n
+        n = n.next
+    end
+
+    @show p
+    @show n
+
+    p = SLLNode{T}()
+    return nothing
+end
+
+size(l::LinkedList) = l.size
+
+function test_slinkedlist(n)
+    println("SLL Time to insert $n nodes:")
+    l = SLinkedList{Float64}(1.0)
+    @time for i in 2:n
+        addlast!(l, convert(Float64, i))
+    end
+
+    println("SLL Time to insert one node in list with $(size(l)) nodes:")
+    @time addlast!(l, 0.0)
+
+    println("DLL Time to insert $n nodes:")
+    l = DLinkedList{Float64}(1.0)
+    @time for i in 2:n
+        addlast!(l, convert(Float64, i))
+    end
+
+    println("Time to insert one node in list with $(size(l)) nodes:")
+    @time addlast!(l, 0.0)
+end
+
+
+"""
+    Stack
+
+A simple stack interface based on Julia Arrays, i.e. dynamic arrays.
+"""
+struct Stack{T <: AbstractVector}
+    data::T
+end
+#Stack{V}() where V = new(Array{V, 1}(undef, 0))
+
+push!(s::Stack{Vector{T}}, x::T) where T = push!(s.data, x); return nothing
+pop!(s::Stack{Vector{T}}) where T = pop!(s.data)#; return nothing
+peek(s::Stack{Vector{T}}) where T = s.data[end];
+size(s::Stack{Vector{T}}) where T = size(s.data)
+
+function test_stack(n)
+    s = Stack(Vector{Float64}(undef, 0))
+
+    @time for i in 1:n
+        push!(s, convert(Float64, i))
+    end
+    
+    @time for i in 1:n
+        pop!(s)
+    end
+end
 
 """
     Node
@@ -26,24 +252,9 @@ mutable struct Node{T <: NodeType}
 end
 
 """
-    Stack
-
-A simple stack interface based on Julia Arrays.
-"""
-mutable struct Stack{T}
-    data::Array{T, 1}
-end
-
-#push!(s::Stack{T}, x::T) where T = push!(s.data, x)#; return nothing
-pop!(s::Stack{T}) where T = pop!(s.data)#; return nothing
-peek(s::Stack{T}) where T = s.data[end];
-size(s::Stack{T}) where T = size(s.data)
-
-
-"""
     RMHeap
 
-Randomised meldable heaps.
+Randomised meldable heaps type.
 """
 mutable struct RMHeap{T <: NodeType}
     root::Node{T}
@@ -52,6 +263,11 @@ end
 
 getmin(item::RMHeap) = item.root
 
+"""
+    add!()
+
+Function to add to the heap.
+"""
 function add!(rmheap::RMHeap{T}, x::T) where {T <: NodeType}
     rmheap.root = meld(rmheap.root, Node{T}(x))
     return nothing
@@ -90,7 +306,7 @@ function meld(node1::Node{T}, node2::Node{T}) where{T <: NodeType}
     return node1
 end
 
-function test(n)
+function test_rmheap(n)
     r = RMHeap{NodeType}()
 
     for i = 1:n
@@ -271,9 +487,7 @@ end
 """
     merge(array1, array2)
 """
-function merge(array1::Array{T, 1}, array2::Array{T, 1}) where T
-    array = Array{T}(undef, length(array1) + length(array2))
-
+function merge!(array, array1::Array{T, 1}, array2::Array{T, 1}) where T
     i = 1; j = 1; k = 1
     while j <= length(array1) && k <= length(array2)
         if array1[j] < array2[k]
@@ -288,7 +502,7 @@ function merge(array1::Array{T, 1}, array2::Array{T, 1}) where T
     end
 
     array[i:end] = [array1[j:end]; array2[k:end]]
-    return array
+    return nothing
 end
 
 """
@@ -301,19 +515,68 @@ function mergesort(array)
 end
 
 function mergesort(array, lo, hi)
-    if hi - lo <= 2
+    if hi - lo <= 60
         return insertionsort(array, lo + 1, hi)[lo:hi]
     else
         mid = lo + (hi - lo) ÷ 2
         array1 = mergesort(array, lo, mid)
         array2 = mergesort(array, mid + 1, hi)
 
-        return merge(array1, array2)
+        array_ = Array{Int}(undef, length(array1) + length(array2))
+        merge!(array_, array1, array2)
+        return array_
     end
+end
+
+function test_mergesort(n)
+    a = rand((0:n), n)
+    println("Merge sort of $n random items:")
+    @time mergesort(a)
+
+    a = rand((0:n), n)
+    println("Built in sort of $n random items:")
+    @time sort(a)
+end
+
+function test_insertionsort(n)
+    a = rand((0:n), n)
+    println("Merge sort of $n random items:")
+    @time insertionsort(a)
+
+    a = rand((0:n), n)
+    println("Built in sort of $n random items:")
+    @time sort(a)
+end
+
 end
 
 module Graph
 
+"""
+    NodeType
 
+Holds types a Node can take.
+"""
+NodeType = Union{T, Nothing} where T
+
+"""
+    Node
+
+Node type.
+"""
+mutable struct Node{T}
+    name::T
+    to::Dict{T, Float64}
+    from::Dict{T, Float64}
+    Node{T}(name) where T = new(name)
+end
+
+# function add(a::Node)
+
+# end
+
+# function test()
+#     a = Node("A", [["B", 1], ["AA", 2]], ["C"])
+# end
 
 end
